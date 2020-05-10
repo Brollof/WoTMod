db6bf4d3087a5c1ef63733a3a415ea8e7454f221
Brollof <thefish3r@gmail.com>
2020-05-10 09:16:18 +0200

    program directories restructure 2/2

--- CWoTbyt/src/CWoTbyt.cpp
+++ CWoTbyt/src/CWoTbyt.cpp
@@ -4,7 +4,15 @@
 #include <iostream>
 #include "TempDownloader.h"
 
+#define FAST_URL 1
+
+#define TEST_URL "https://www.w3.org/TR/PNG/iso_8859-1.txt"
 #define XVM_URL "https://nightly.modxvm.com/download/master/xvm_latest.zip"
+
+#if (FAST_URL == 1)
+#undef XVM_URL
+#define XVM_URL TEST_URL
+#endif
 
 
 int main()
