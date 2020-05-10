18e89de352733561a37e6eb36205f37a807a4717
Brollof <thefish3r@gmail.com>
2020-05-10 09:31:10 +0200

    Common.h file added

---
+++ CWoTbyt/inc/Common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define TEST_URL "https://www.w3.org/TR/PNG/iso_8859-1.txt"
+#define XVM_URL "https://nightly.modxvm.com/download/master/xvm_latest.zip"
+
+#ifndef FAST_URL
+#define FAST_URL 0
+#endif
+
+#if (FAST_URL == 1)
+#undef XVM_URL
+#define XVM_URL TEST_URL
+#endif
--- CWoTbyt/src/CWoTbyt.cpp
+++ CWoTbyt/src/CWoTbyt.cpp
@@ -1,27 +1,19 @@
-// CWoTbyt.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Common.h"
 #include "TempDownloader.h"
 
-#define FAST_URL 1
-
-#define TEST_URL "https://www.w3.org/TR/PNG/iso_8859-1.txt"
-#define XVM_URL "https://nightly.modxvm.com/download/master/xvm_latest.zip"
-
-#if (FAST_URL == 1)
-#undef XVM_URL
-#define XVM_URL TEST_URL
-#endif
-
+#define SKIP_DOWNLOAD
 
 int main()
 {
+#ifndef SKIP_DOWNLOAD
 	TempDownloader dl = TempDownloader(XVM_URL);
 	if (dl.Download())
 	{
 		return 1;
 	}
-	
+#endif
+
+
 	
 }
