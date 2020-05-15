0f37b7e10d48b3b48eb648edd54a566bab82de74
Brollof <thefish3r@gmail.com>
2020-05-15 22:22:39 +0200

    log file name changed; file description changed

--- CWoTbyt/inc/Log.h
+++ CWoTbyt/inc/Log.h
@@ -9,7 +9,7 @@
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 #define LOGGER_NAME         "main"
-#define LOG_FILE            "wotbyt.log"
+#define LOG_FILE            "cwotbyt.log"
 #define LOG_PATTERN_REL     "[%d/%m/%Y %T.%e] %v"
 #define LOG_PATTERN_DBG     "[%d/%m/%Y %T.%e] [%l] %v"
 
