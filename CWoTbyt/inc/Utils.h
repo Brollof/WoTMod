a2c4877ac5ad6e0c83a5c57cd8962922227bd0b9
Brollof <thefish3r@gmail.com>
2020-05-15 23:29:48 +0200

    fixed 'press any key' behavior

--- CWoTbyt/inc/Utils.h
+++ CWoTbyt/inc/Utils.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <codecvt>
 #include <AtlBase.h>
+#include "conio.h"
 
 #define STR_TO_WSTR(str) std::wstring(CA2W(str))
 #define WSTR_TO_STR(str) std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(str)
@@ -10,5 +11,5 @@
 static inline void AppEnd()
 {
   std::cout << "Press any key to exit...";
-  std::cin.get();
+  _getch();
 }
