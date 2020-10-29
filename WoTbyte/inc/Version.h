6d8ce46ffe8c708647ea063fc949b742d4f850c9
Brollof <thefish3r@gmail.com>
2020-10-29 15:20:24 +0100

    version updated to 2.0.0

--- WoTbyte/inc/Version.h
+++ WoTbyte/inc/Version.h
@@ -3,8 +3,8 @@
 #define STRINGIZE2(s)               #s
 #define STRINGIZE(s)                STRINGIZE2(s)
 
-#define VERSION_MAJOR               1
-#define VERSION_MINOR               4
+#define VERSION_MAJOR               2
+#define VERSION_MINOR               0
 #define VERSION_REVISION            0
 
 #define VER_FILE_DESCRIPTION_STR    "WoTbyte"
